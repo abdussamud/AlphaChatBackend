919fc389fa869e190c820e78b2f305823e7c6610
AlphaNineOne <canninew@gmail.com>
2025-09-06 23:14:13 +0500

    db change

--- server/coresite/settings/database_config_setting.py
+++ server/coresite/settings/database_config_setting.py
@@ -1,12 +1,23 @@
-from .environment_setting import env
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('PASSWORD'),
-        'HOST': env('HOST'),
-        'PORT': 5432,
-        'ATOMIC_REQUESTS': True,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# from .environment_setting import env
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('DATABASE_NAME'),
+#         'USER': env('DATABASE_USER'),
+#         'PASSWORD': env('PASSWORD'),
+#         'HOST': env('HOST'),
+#         'PORT': 5432,
+#         'ATOMIC_REQUESTS': True,
+#     }
+# }
